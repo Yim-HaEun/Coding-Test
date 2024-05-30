388a00a251a01dcf23c3e0fc2667322132275a14
Yim-HaEun <49932613+Yim-HaEun@users.noreply.github.com>
2024-05-31 03:10:08 +0900

    [level 0] Title: 수 조작하기 1, Time: 0.93 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int n, String control) {
+        
+        char[] arr = control.toCharArray();
+        for(int i =0; i < arr.length; i++){
+            switch(arr[i]){
+                case 'w': n+=1;
+                    break;
+                case 's': n-=1;
+                    break;
+                case 'd': n+=10;
+                    break;
+                case 'a': n-=10;
+                    break;       
+            }
+        }
+        return n;
+    }
+}
