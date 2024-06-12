17cc7ab41db0de50ceb0116c64c37322dae98aec
Yim-HaEun <49932613+Yim-HaEun@users.noreply.github.com>
2024-06-12 15:44:27 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 5.05 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        
+        myString = myString.toLowerCase();
+        pat = pat.toLowerCase();
+        int answer = myString.contains(pat) ?  1: 0;
+        return answer;
+    }
+}
