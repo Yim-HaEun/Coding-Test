dc6616c7d8496a3ac5f95b65793476d3be8e604f
Yim-HaEun <49932613+Yim-HaEun@users.noreply.github.com>
2024-06-13 16:39:15 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.32 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
@@ -2,8 +2,8 @@ class Solution {
     public int solution(String num_str) {
         int answer = 0;
         String[] str = num_str.split("");
-        for(int i=0;i<str.length;i++){
-            answer+= Integer.parseInt(str[i]);
+        for(String n : str){
+            answer+= Integer.parseInt(n);
         }
         return answer;
     }
