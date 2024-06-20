e2c77cf2892d6c6a10cfc522aa5ce987bfdc2419
Yim-HaEun <49932613+Yim-HaEun@users.noreply.github.com>
2024-06-20 16:17:00 +0900

    [level 0] Title: 배열 만들기 6, Time: 20.48 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] solution(int[] arr) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int no : arr) {
+            if (!stack.isEmpty() && no == stack.peek()) {
+                stack.pop();
+            } else {
+                stack.push(no);
+            }
+        }
+
+        return stack.isEmpty() ? new int[] { -1 } : stack.stream().mapToInt(i -> i).toArray();
+    }
+}
