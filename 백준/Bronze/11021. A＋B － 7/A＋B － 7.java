da346b40b37ffb5733a40852b8244dcb6ed5cfed
Yim-HaEun <49932613+Yim-HaEun@users.noreply.github.com>
2024-09-21 03:46:09 +0900

    [Bronze V] Title: A+B - 7, Time: 128 ms, Memory: 14624 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader bufferedReader = new BufferedReader (new InputStreamReader(System.in));//선언
+		BufferedWriter bufferedWriter = new BufferedWriter (new OutputStreamWriter(System.out)); //선언
+		
+		int n = Integer.parseInt(bufferedReader.readLine()); //입력받기 
+		for(int i=0;i<n;i++) {
+			String[] input = bufferedReader.readLine().split(" "); //한줄 입력
+			int a = Integer.parseInt(input[0]);
+			int b = Integer.parseInt(input[1]);
+			bufferedWriter.write(String.format("Case #%d: %d",i+1,a+b)); //출력
+			bufferedWriter.newLine(); //개행
+			bufferedWriter.flush();//버퍼 전부 출력 (필수)
+		}
+		
+		bufferedWriter.close();//스트림 닫기 (필수)
+	}	
+}
