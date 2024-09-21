e54801d388e81f15a4701f56992e971a5745fec3
Yim-HaEun <49932613+Yim-HaEun@users.noreply.github.com>
2024-09-22 01:53:11 +0900

    [Bronze V] Title: 사파리월드, Time: 176 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		long a = scan.nextLong();
+		long b = scan.nextLong();
+		
+		long answer = Math.abs(a-b);
+		System.out.println(answer);
+	
+	
+	}
+}
