378df2019e9b0b847ad9f82ce4996bee4df174d1
Yim-HaEun <49932613+Yim-HaEun@users.noreply.github.com>
2024-09-23 01:07:02 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 172 ms, Memory: 17544 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args){
+	Scanner scan = new Scanner(System.in);
+	String str = scan.next();
+	if(str.equals("NLCS")) {
+		System.out.println("North London Collegiate School");
+	}else if(str.equals("BHA")) {
+		System.out.println("Branksome Hall Asia");
+	}else if(str.equals("KIS")) {
+		System.out.println("Korea International School");
+	}else if(str.equals("SJA")) {
+		System.out.println("St. Johnsbury Academy");
+	}
+		
+	scan.close();
+	
+	}
+}
