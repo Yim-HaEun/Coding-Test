ad486d4cabd9c74c3d4476cbdd70ca03e3e14db6
Yim-HaEun <49932613+Yim-HaEun@users.noreply.github.com>
2024-09-23 02:43:58 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 164 ms, Memory: 17548 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class Main {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		String str = scan.next();
+		switch(str) {
+		case "SONGDO": System.out.println("HIGHSCHOOL");break; 
+		case "CODE": System.out.println("MASTER");break; 
+		case "2023": System.out.println("0611");break; 
+		case "ALGORITHM": System.out.println("CONTEST");break; 
+		
+		default: break;
+		}
+		scan.close();
+	}
+}
