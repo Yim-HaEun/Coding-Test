b0023adfe27f1cd4a2ff275bf7e0707c3786ac95
Yim-HaEun <49932613+Yim-HaEun@users.noreply.github.com>
2024-09-26 02:41:47 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 168 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        double r = scan.nextInt();
+       
+       System.out.println ((2*a)+(2*3.141592*r));
+       	
+    }
+    
+}
